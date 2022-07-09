1dd8a473ccbd40c793daa9acfc02207060ee8caa
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-07-09 22:58:36 +0530

    fixed typo

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { SubHeading, MenuItem } from "../../components";
+import { SubHeading } from "../../components";
 import { images, data } from "../../constants";
 import "./Laurels.css";
 
