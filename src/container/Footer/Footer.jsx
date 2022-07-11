5d3b6e009bee659dda3122b509213b035430629e
Sanidhya Kr. Verma <sanidhya.verma12345@gmail.com>
2022-07-11 20:47:04 +0530

    fixed copright year

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -51,7 +51,9 @@ const Footer = () => (
     </div>
 
     <div className="footer__copyright">
-      <p className="p__opensans">2021 Gerîcht. All Rights Reserved.</p>
+      <p className="p__opensans">
+        {new Date().getFullYear()} Gerîcht. All Rights Reserved.
+      </p>
     </div>
   </div>
 );
